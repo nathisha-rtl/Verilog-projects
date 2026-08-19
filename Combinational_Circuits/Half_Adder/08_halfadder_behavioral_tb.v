cfa3278df8393393367c6c910256cf1b6fadcbcc
Nathisha Raja <nathisharaja126@gmail.com>
2026-08-19 20:29:05 +0530

    Add behavioral testbench for half adder

---
+++ Combinational_Circuits/Half_Adder/08_halfadder_behavioral_tb.v
@@ -0,0 +1,20 @@
+module halfadder_tb;
+  reg A,B ;
+  wire sum,carry;
+  
+  halfadder uut(.A(A),
+             .B(B),
+             .sum(sum),
+             .carry(carry));
+    initial begin 
+      $dumpfile("halfadder_tb.vcd");
+      $dumpvars(0,halfadder_tb);
+      
+      $monitor("$time=%0t ; A=%b ; B=%b ; sum=%b ; carry=%b ", $time , A , B , sum , carry );
+               A=0;B=0;#10;
+               A=0;B=1;#10;
+               A=1;B=0;#10;
+               A=1;B=1;#10;
+               $finish;
+               end
+               endmodule
