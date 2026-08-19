84b300ba86a97d1682f9be55f9c2d0b113d88384
Nathisha Raja <nathisharaja126@gmail.com>
2026-08-19 21:07:20 +0530

    Add behavioral testbench for full adder

---
+++ Combinational_Circuits/Full_Adder/08_fulladder_tb_behavioral.v
@@ -0,0 +1,35 @@
+module full_adder_tb;
+
+reg A, B, Cin;
+wire Sum, Carry;
+
+full_adder uut (
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial begin
+
+    $dumpfile("full_adder.vcd");
+    $dumpvars(0, full_adder_tb);
+
+    $monitor("time=%0t A=%b B=%b Cin=%b Sum=%b Carry=%b",
+              $time, A, B, Cin, Sum, Carry);
+
+    A=0; B=0; Cin=0;
+    #10 A=0; B=0; Cin=1;
+    #10 A=0; B=1; Cin=0;
+    #10 A=0; B=1; Cin=1;
+    #10 A=1; B=0; Cin=0;
+    #10 A=1; B=0; Cin=1;
+    #10 A=1; B=1; Cin=0;
+    #10 A=1; B=1; Cin=1;
+
+    #10 $finish;
+
+end
+
+endmodule
