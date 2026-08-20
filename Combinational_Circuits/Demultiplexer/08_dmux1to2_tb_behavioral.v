c958d80e2a2a57daf25c8d171bad7a9e5909745e
Nathisha Raja <nathisharaja126@gmail.com>
2026-08-20 20:23:05 +0530

    Create 08_dmux1to2_tb_behavioral.v

---
+++ Combinational_Circuits/Demultiplexer/08_dmux1to2_tb_behavioral.v
@@ -0,0 +1,30 @@
+module demux1to2_tb;
+
+  reg S, D;
+  wire Y0, Y1;
+
+  demux1to2 uut (
+    .S(S),
+    .D(D),
+    .Y0(Y0),
+    .Y1(Y1)
+  );
+
+  initial begin
+
+    $dumpfile("demux1to2_tb.vcd");
+    $dumpvars(0, demux1to2_tb);
+
+    $monitor("time=%0t ; S=%b ; D=%b ; Y0=%b ; Y1=%b",
+             $time, S, D, Y0, Y1);
+
+    S=0; D=0; #10;
+    S=0; D=1; #10;
+    S=1; D=0; #10;
+    S=1; D=1; #10;
+
+    $finish;
+
+  end
+
+endmodule
