fe15bde8052ff54d947b29e99366f2777b1b1b23
Nathisha Raja <nathisharaja126@gmail.com>
2026-08-23 12:03:05 +0530

    Create 03_comparator1bit_behavioral.v

---
+++ Combinational_Circuits/Comparator/03_comparator1bit_behavioral.v
@@ -0,0 +1,33 @@
+// 1-bit Comparator using behavioral Modeling
+
+module comparator(
+    input A,
+    input B,
+    output reg A_greater_B,
+    output reg A_equal_B,
+  output reg A_less_B);
+  
+  always @(*) begin
+    if(A>B) begin
+        A_greater_B= 1;
+        A_equal_B  = 0;
+       A_less_B   = 0;
+    end
+    
+    else if (A == B) begin
+        A_greater_B = 0;
+        A_equal_B   = 1;
+        A_less_B    = 0;
+    end
+
+    else begin
+        A_greater_B = 0;
+        A_equal_B   = 0;
+        A_less_B    = 1;
+    end
+
+end
+
+endmodule
+      
+
