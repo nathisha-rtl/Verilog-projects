8c13a88ff36b829d07d814192a9793d9b68dc68c
Nathisha Raja <nathisharaja126@gmail.com>
2026-09-09 20:47:00 +0530

    Create 16_ring_counter.v

---
+++ Combinational_Circuits/Counters/16_ring_counter.v
@@ -0,0 +1,13 @@
+module ring_counter(input clk ,reset ,
+                    output reg[3:0]Q);
+  always @(posedge clk or posedge reset) begin
+    if(reset==1)
+      Q<=4'b1000;
+    else begin
+      Q[3]<=Q[0];
+      Q[2]<=Q[3];
+      Q[1]<=Q[2];
+      Q[0]<=Q[1];
+    end
+  end
+endmodule
